e7eb165ff2505938b39f51e567ed52342a13aa69
Darshak Patel <102433527+TheShak5@users.noreply.github.com>
2023-05-02 18:18:16 -0400

    redoing it all to ensure responsiveness works

---
+++ assets/js/responsive.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  document
+  .getElementById('nav_toggle')
+  .addEventListener('click', (e) => e.target.classList.toggle('hide-mobile'))
+});
