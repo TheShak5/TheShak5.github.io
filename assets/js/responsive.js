e3cb08e495385e0c73168abdc83524a907611996
Darshak Patel <102433527+TheShak5@users.noreply.github.com>
2023-05-10 11:03:48 -0400

    trying something out for mobile images

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -3,3 +3,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
   .getElementById('nav_toggle')
   .addEventListener('click', (e) => e.target.classList.toggle('hide-mobile'))
 });
+
+const images = document.querySelectorAll('.image');
+const newImage = document.querySelector('.mobile-image');
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 1000) {
+    images.forEach(image => {
+      image.style.display = 'none';
+    });
+    newImage.style.display = 'block';
+  } else {
+    images.forEach(image => {
+      image.style.display = 'block';
+    });
+    newImage.style.display = 'none';
+  }
+});
+
